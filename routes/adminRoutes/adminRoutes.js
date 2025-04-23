f7542f611a78276c8ae82b55268a290d42c7c5fb
RajavrukshaGroups <technical@rajavrukshagroup.in>
2025-04-23 17:23:08 +0530

    edit changes

--- routes/adminRoutes/adminRoutes.js
+++ routes/adminRoutes/adminRoutes.js
@@ -1,7 +1,7 @@
-import express from 'express';
-import adminController from '../../controller/adminController/adminController.js';
-import Room from '../../Models/Room.js';
-const router =express.Router()
+import express from "express";
+import adminController from "../../controller/adminController/adminController.js";
+import Room from "../../Models/Room.js";
+const router = express.Router();
 
 router.post("/adminLogin", adminController.Adminlogin);
 // router.get("/roomsavailable", adminController.GetRoomsData);
@@ -27,15 +27,15 @@ router.get(
   "/bookings/filter-checkin-dates",
   adminController.CollectAllUniqueCheckInDates
 );
-router.get("/roomsdata",adminController.getRoomsData)
-router.post('/rooms',adminController.saveRoomData)
-router.put("/editSaveroom/:id",adminController.editSaveroom)
+router.get("/roomsdata", adminController.getRoomsData);
+router.post("/rooms", adminController.saveRoomData);
+router.put("/editSaveroom/:id", adminController.editSaveroom);
 router.delete("/deleteroom/:id", adminController.deleteRoomById);
-router.post("/addRoomtype",adminController.addRoomType);
-router.get("/getRoomtype",adminController.getRoomType);
-router.put("/updateroomtype/:id",adminController.updateRoomType);
-router.delete("/deleteroomtype/:id",adminController.deleteRoomType);
+router.post("/addRoomtype", adminController.addRoomType);
+router.get("/getRoomtype", adminController.getRoomType);
+router.put("/updateroomtype/:id", adminController.updateRoomType);
+router.delete("/deleteroomtype/:id", adminController.deleteRoomType);
 router.get("/checkroomsavailability", adminController.GetRoomsData);
+router.get("/indbookingId/:id", adminController.GetIndividualBookingData);
 
-  export default router
-  
+export default router;
